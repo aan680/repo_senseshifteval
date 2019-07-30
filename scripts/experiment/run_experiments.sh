42aba1289b83583092bd7844a1a4379d650cacc2
Astrid van Aggelen <astrid.van.aggelen@cwi.nl>
2019-07-30 19:48:21 +0200

    last checkin of day

--- scripts/experiment/run_experiments.sh
+++ scripts/experiment/run_experiments.sh
@@ -4,7 +4,7 @@ datafolder="/ufs/aggelen/SenseShiftEval/data/"
 mkdir ./results
 
 Rscript scripts/experiment/get_correlations_simplified.R --dataset="HW+" --inputfile="$datafolder/HW/gold_wordpair_after_iaa.csv"
-Rscript scripts/experiment/get_correlations_simplified.R --dataset="HT" --inputfile="$datafolder/HT/gold_old_and_new.csv" 
-Rscript scripts/experiment/get_correlations_simplified.R --dataset="WSCT" --inputfile="$datafolder/WSCT/gold_wordpair_after_iaa.csv"
-Rscript scripts/experiment/get_correlations_simplified.R --dataset="HW" --inputfile="$datafolder/HW/gold_hamilton_28.csv" --add_stats=FALSE #do not add_stats because no WN annotations
+#Rscript scripts/experiment/get_correlations_simplified.R --dataset="HT" --inputfile="$datafolder/HT/gold_old_and_new.csv" 
+#Rscript scripts/experiment/get_correlations_simplified.R --dataset="WSCT" --inputfile="$datafolder/WSCT/gold_wordpair_after_iaa.csv"
+#Rscript scripts/experiment/get_correlations_simplified.R --dataset="HW" --inputfile="$datafolder/HW/gold_hamilton_28.csv" --add_stats=FALSE #do not add_stats because no WN annotations
 
