a4643e348d7811112f5212cb32b823eab31e7076
Astrid van Aggelen <astrid.van.aggelen@cwi.nl>
2019-07-31 17:34:09 +0200

    necessary modifications

--- scripts/experiment/hw_functions.py
+++ scripts/experiment/hw_functions.py
@@ -18,9 +18,9 @@ sys.path.insert(0, histwords_dir)
 from representations.sequentialembedding import SequentialEmbedding
 
 
-embeddingfile="embeddings/eng-all_sgns"
+#embeddingfile="embeddings/eng-all_sgns"
 
-def rho_p_correct(target, ref, gold, t):
+def evaluate_by_hw(target, ref, t, gold, embeddingfile):
     gold   = int(gold)
     offset =  int(t / 10) * 10 #round off to nearest smaller decade, like I did in my R script get_correlations
     #print(offset)
@@ -29,29 +29,19 @@ def rho_p_correct(target, ref, gold, t):
     t = collections.OrderedDict([])
     for y in range(offset, 2000, 10): t[y]=time_sims[y]
     rho, p = scipy.stats.spearmanr(t.keys(), t.values())
+    if p <= 0.05: 
+	sig = 1
+    else:
+        sig = 0
     #print(t)
     if np.isnan(rho):
 	correct = float('nan')
+	sig = float('nan')
     elif np.sign(rho) == np.sign(gold):
 	correct = 1
     else:
 	correct = 0
-    return rho, p, correct
+    return {"correct":correct, 'sig':sig, "p":p, "corr":rho}
 
-def correct_by_hw(target, ref, gold, t):
-    gold   = int(gold)
-    offset =  int(t / 10) * 10 #round off to nearest smaller decade, like I did in my R script get_correlations
-    #print(offset)
-    embeddings = SequentialEmbedding.load(embeddingfile, range(1800, 2000, 10))
-    time_sims = embeddings.get_time_sims(target, ref)
-    t = collections.OrderedDict([])
-    for y in range(offset, 2000, 10): t[y]=time_sims[y]
-    rho, p = scipy.stats.spearmanr(t.keys(), t.values())
-    #print(t)
-    if np.isnan(rho):
-	correct = float('nan')
-    elif np.sign(rho) == np.sign(gold):
-	correct = 1
-    else:
-	correct = 0
-    return correct
+
+
